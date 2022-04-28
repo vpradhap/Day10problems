7e7dd99d931548139e4d8851704fccea9bf2e912
vpradhap <pradhapvenkat@gmail.com>
2022-04-28 09:37:27 +0530

    code changed to see percentage of singlet combination

--- flipcoinCombination.sh
+++ flipcoinCombination.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
-coin_toss=$((RANDOM%2))
+counter=0
+total_heads=0
+total_tails=0
 
-if [ $coin_toss -eq 0 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A singlet
+
+singlet[head]=" "
+singlet[tails]=" "
+singlet[h_per]=0
+singlet[t_per]=0
+
+
+read -p "Enter how many times you want flip the coin  " FLIP
+
+while [ $counter -ne $FLIP ]
+do
+	coin_1=$((RANDOM%2))
+
+	if [ $coin_1 -eq 1 ]
+	then
+		singlet[head]="H"
+		((total_heads++))
+	else
+		singlet[tails]="T"
+      ((total_tails++))
+	fi
+	((counter++))
+done
+
+
+echo "Singlet Combination" ${singlet[head]} ${singlet[tails]}
+
+singlet[h_per]=$(echo $total_heads $FLIP | awk '{print ($1/$2)*100}')
+echo "Percentage of Head ="${singlet[h_per]}
+
+singlet[t_per]=$(echo $total_tails $FLIP | awk '{print ($1/$2)*100}')
+echo "Percentage of Tail =" ${singlet[t_per]}
 
